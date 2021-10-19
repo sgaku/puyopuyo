7bc9a0412b0a0d03e79ac8bf861f3a06e0e13974
Gaku Sato <ggg514@icloud.com>
2021-10-19 17:12:34 +0900

    骨組みは完成

--- Assets/script/PuyoMove.cs
+++ Assets/script/PuyoMove.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class PuyoMove : MonoBehaviour
 {
-   // private float previousTime;
-  //  public float fallTime = 5f;
+    private float previousTime;
 
+    GameObject ghostpuyos;
     public static  int width = 6, height = 13;
     public static GameObject[,] grid = new GameObject[width, height];
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreatGhostPuyos();
+        previousTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
+        MoveGhost();
+
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.position += new Vector3(-1, 0, 0);
@@ -34,19 +37,21 @@ public class PuyoMove : MonoBehaviour
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - previousTime >0.6)
         {
             transform.position += new Vector3(0, -1, 0);
             if (!ValidMove())
             {
                 transform.position -= new Vector3(0, -1, 0);
                 AddGrid();
+                Destroy(ghostpuyos);
                 this.gameObject.transform.DetachChildren();
                 Destroy(this.gameObject);
                 this.enabled = false;
                 
-                FindObjectOfType<GameManager>().CreatePuyos();
+                FindObjectOfType<GameManager>().DropPuyo();
             }
+            previousTime = Time.time;
         } else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             transform.RotateAround(transform.position, new Vector3(0, 0, 1), 90);
@@ -59,13 +64,61 @@ public class PuyoMove : MonoBehaviour
 
     void AddGrid()
     {
-        foreach (GameObject childblocks in transform)
+        foreach (Transform childblocks in transform)
         {
             int roundX = Mathf.RoundToInt(childblocks.transform.position.x);
             int roundY = Mathf.RoundToInt(childblocks.transform.position.y);
 
-            grid[roundX, roundY] = childblocks;
+            grid[roundX, roundY] = childblocks.gameObject;
         }
+    }
+
+
+    void CreatGhostPuyos()
+    {
+        ghostpuyos = Instantiate(this.gameObject);
+        ghostpuyos.transform.position = this.gameObject.transform.position;
+        Destroy(ghostpuyos.GetComponent<PuyoMove>());
+        foreach(Transform children in ghostpuyos.transform)
+        {
+            children.GetComponent<SpriteRenderer>().color -= new Color(0,0,0,0.5f);
+            children.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        }
+    }
+
+    void MoveGhost()
+    {
+        ghostpuyos.transform.position = this.gameObject.transform.position;
+        ghostpuyos.transform.rotation = this.gameObject.transform.rotation;
+
+        while (ValidMoveGhost())
+        {
+            ghostpuyos.transform.position += new Vector3(0, -1, 0);
+        }
+        if (!ValidMoveGhost())
+        {
+            ghostpuyos.transform.position += new Vector3(0, 1, 0);
+        }
+    }
+    bool ValidMoveGhost()
+    {
+        foreach (Transform childblocks in ghostpuyos.transform)
+        {
+            int roundX = Mathf.RoundToInt(childblocks.transform.position.x);
+            int roundY = Mathf.RoundToInt(childblocks.transform.position.y);
+
+            if (roundX < 0 || roundX >= width || roundY < 0 || roundY > height)
+            {
+                return false;
+            }
+
+            if (grid[roundX, roundY] != null)
+            {
+                return false;
+            }
+
+        }
+        return true;
     }
 
 
